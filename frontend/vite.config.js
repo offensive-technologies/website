bc9c1de035805146b15665614d000ed7918c9d60
Sarah Bader <bader.sarah@icloud.com>
2025-04-27 01:57:58 +0200

    updated vite configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
+// frontend/vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website/',  
-  build: {
-    outDir: 'build', 
-  },
+  plugins: [react()],
+  base: '/',  
 });
